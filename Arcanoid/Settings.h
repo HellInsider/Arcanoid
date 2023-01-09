2c06470d0d46d2d14f80fc466a99614e8c00020a
meso_test <lfyz20012@mail.ru>
2023-01-09 14:31:40 +0300

    Mpving files to git

---
+++ Arcanoid/Settings.h
@@ -0,0 +1,21 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+
+#include "SFML/Graphics.hpp"
+#include "SFML/Audio.hpp"
+
+
+#define FPS 120
+#define DisplayX 1021
+#define DisplayY 859
+#define PI (float)(3.1416)
+
+using namespace sf;
+
+
+#define MaxBalls  10
+#define MaxBonuses  12
+
+
+#endif SETTINGS_H
